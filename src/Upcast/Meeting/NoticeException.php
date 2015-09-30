3f5d99ab3f4fb5b8b26e9b0dace6b5211c56a13b
Lee Davis <leedavis81@hotmail.com>
2015-09-30 13:16:50 +0100

    Add more

---
+++ src/Upcast/Meeting/NoticeException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 30/09/15
+ * Time: 11:23
+ */
+
+namespace Upcast\Meeting;
+
+
+trait NoticeException {
+
+}
---
+++ tests/UpcastTests/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lee
+ * Date: 30/09/15
+ * Time: 11:12
+ */
